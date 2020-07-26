e8adb01ae6b8c9d289fda31bd0e485a33183dde0
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-07-26 22:12:39 +0530

    .

--- core/models.py
+++ core/models.py
@@ -46,29 +46,29 @@ class Item(models.Model):
 
 class OrderItem(models.Model):
     user = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE,
-            blank=True,
-            null=True
-        )
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    
+
     def __str__(self):
         return "{} of {}".format(self.quantity,self.item.item_name)
 
 
 class Order(models.Model):
     user = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE
-        )
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     ordered = models.BooleanField(default=False)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
-    
+
 
     def __str__(self):
         return self.user.username
